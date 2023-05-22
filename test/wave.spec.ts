8c9e0c3e88525c22cdcf3da7b3525efea1683fbd
SieR-VR <nwh040512@gmail.com>
2023-05-22 03:46:16 +0000

    add test configs

--- test/wave.spec.ts
+++ test/wave.spec.ts
@@ -1,3 +1,6 @@
+import { writeFileSync } from "node:fs";
+import { createServer } from "vite";
+
 import { Wave, WaveRaw, mergeWave } from "../src/wave";
 import { makeSingleSinWave } from "./util";
 
@@ -5,6 +8,18 @@ describe('wave', () => {
     it('make wave with zero', () => {
         const wave = makeSingleSinWave(100, 0, 100);
 
-        expect(wave.data);
+        expect(wave).toBeDefined();
+        expect(wave.data).toBeDefined();
+    });
+
+    it('make two wave and merge', () => {
+        const wave1 = makeSingleSinWave(100, 10, 100);
+        const wave2 = makeSingleSinWave(50, 20, 100);
+
+        const merged = mergeWave(wave1, wave2);
+
+        expect(merged).toBeDefined();
+        expect(merged.data).toBeDefined();
+        expect(merged.data.length).toBe(wave1.data.length);
     });
 });
